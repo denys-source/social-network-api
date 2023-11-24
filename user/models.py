87242b547461fa83c5ae644bb260ccdd7e160a8c
Denys <deibukdenyscloud@gmail.com>
2023-11-24 16:32:47 +0200

    Create Post and Like models

--- user/models.py
+++ user/models.py
@@ -2,4 +2,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    ...
+    @property
+    def name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
