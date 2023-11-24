a351bdfca017752b394fc6663c5c712328898cd2
Denys <deibukdenyscloud@gmail.com>
2023-11-24 15:22:20 +0200

    Create and register custom user model

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "social_app",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -110,3 +112,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
