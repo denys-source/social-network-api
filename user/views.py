a8741546a2446a1bfce2d5b894d1589ca9febb9b
Denys <deibukdenyscloud@gmail.com>
2023-11-24 17:17:10 +0200

    Add user registration

--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    model = get_user_model()
+    serializer_class = UserSerializer
